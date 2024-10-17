64ac1730752a9c4969ba590cee9484bb4a189143
scui24 <123330983+scui24@users.noreply.github.com>
2024-10-16 20:11:33 -0500

    actor

--- actor.py
+++ actor.py
@@ -1,21 +1,31 @@
-# Importing the libraries
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import csv
 import re
 
-# Importing the dataset
 dataset = pd.read_csv('subset.csv')
 
 found = []
+nominees = []
+freq = []
 for i in range(0, len(dataset['text'])):
 	if (type(dataset['text'][i]) is not type("str")):
 	    print(int(i + 2))
 	    corpus.append("error")
 	    continue
+
 	pattern = r"best actor"
-	name_pattern = r"[A-Z][a-zA-Z'-]+(?:\s[A-Z][a-zA-Z'-]+)*"
+	name_pattern = r"[A-Z][a-zA-Z]*\s[A-Z][a-zA-Z]*(?:\s[A-Z][a-zA-Z]*)*"
 	match = re.search(pattern, dataset['text'][i], re.IGNORECASE)
 	if match:
 		found.append(dataset['text'][i])
+		name_matches = re.findall(name_pattern, dataset['text'][i])
+		for name in name_matches:
+			if name in nominees:
+				freq[nominees.index(name)] += 1
+			else:
+				nominees.append(name)
+				freq.append(1)
+
+print(nominees)
+print(freq)
+print(len(found))
