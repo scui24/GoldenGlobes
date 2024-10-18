e6f3b8235a71a3fc1aed0d2f942ae6354427ac94
inessa <verbitskyinessa@gmail.com>
2024-10-18 16:27:17 -0500

    added get_awards function

--- gg_api.py
+++ gg_api.py
@@ -9,8 +9,209 @@ def get_hosts(year):
 def get_awards(year):
     '''Awards is a list of strings. Do NOT change the name
     of this function or what it returns.'''
-    # Your code here
-    return awards
+    people = []
+    category = ...
+    sub_category = ...
+    sub_sub_category = ...
+    sub_sub_sub_category = ...
+    if category == "tv_show":
+
+
+        if sub_category == "best original song - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best motion picture - drama":
+            people.append(sub_category)
+        elif sub_category == "best television limited series, anthology series, or motion picture made for television":
+            people.append(sub_category)
+        elif sub_category == "best television series":
+            if sub_sub_category == "musical or comedy":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "drama":
+                people.append(sub_category + " - " + sub_sub_category)
+
+        elif sub_category == "best motion picture":
+            if sub_sub_category == "non-english language":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "animated":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "musical or comedy":
+                people.append(sub_category + " - " + sub_sub_category)
+
+        elif sub_category == "best screenplay - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best director - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best original score":
+            people.append(sub_category)
+        elif sub_category == "best performance":
+            if sub_sub_category =="by an actor":
+                if sub_sub_sub_category == "in a suporting role in a series, limited series, or motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a limited series, anthology series, or a motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+            elif sub_sub_category == "by an actress":
+                if sub_sub_sub_category == "in a suporting role in a series, limited series, or motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a limited series, anthology series, or a motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+
+   
+     
+
+            
+
+    elif category == "people":
+        if sub_category == "best original song - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best motion picture - drama":
+            people.append(sub_category)
+        elif sub_category == "best television limited series, anthology series, or motion picture made for television":
+            people.append(sub_category)
+        elif sub_category == "best television series":
+            if sub_sub_category == "musical or comedy":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "drama":
+                people.append(sub_category + " - " + sub_sub_category)
+
+        elif sub_category == "best motion picture":
+            if sub_sub_category == "non-english language":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "animated":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "musical or comedy":
+                people.append(sub_category + " - " + sub_sub_category)
+
+        elif sub_category == "best screenplay - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best director - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best original score":
+            people.append(sub_category)
+        elif sub_category == "best performance":
+            if sub_sub_category =="by an actor":
+                if sub_sub_sub_category == "in a suporting role in a series, limited series, or motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a limited series, anthology series, or a motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+            elif sub_sub_category == "by an actress":
+                if sub_sub_sub_category == "in a suporting role in a series, limited series, or motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a limited series, anthology series, or a motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+
+    elif category == "film":
+        if sub_category == "best original song - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best motion picture - drama":
+            people.append(sub_category)
+        elif sub_category == "best television limited series, anthology series, or motion picture made for television":
+            people.append(sub_category)
+        elif sub_category == "best television series":
+            if sub_sub_category == "musical or comedy":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "drama":
+                people.append(sub_category + " - " + sub_sub_category)
+
+        elif sub_category == "best motion picture":
+            if sub_sub_category == "non-english language":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "animated":
+                people.append(sub_category + " - " + sub_sub_category)
+            elif sub_sub_category == "musical or comedy":
+                people.append(sub_category + " - " + sub_sub_category)
+
+        elif sub_category == "best screenplay - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best director - motion picture":
+            people.append(sub_category)
+        elif sub_category == "best original score":
+            people.append(sub_category)
+        elif sub_category == "best performance":
+            if sub_sub_category =="by an actor":
+                if sub_sub_sub_category == "in a suporting role in a series, limited series, or motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a limited series, anthology series, or a motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+            elif sub_sub_category == "by an actress":
+                if sub_sub_sub_category == "in a suporting role in a series, limited series, or motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a limited series, anthology series, or a motion picture made for":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a television series - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a supporting role in any motion picture":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - musical or comedy":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+                if sub_sub_sub_category == "in a motion picture - drama":
+                    people.append(sub_category + sub_sub_category + sub_sub_sub_category)
+
+       
+    return people
 
 def get_nominees(year):
     '''Nominees is a dictionary with the hard coded award
